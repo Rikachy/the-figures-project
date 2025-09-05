6028f800262d389ca88bc340da82d8fd82f51296
Richard <richardlife101@gmail.com>
2025-09-05 10:16:48 -0400

    Added the person walking up the house.

--- Picture.java
+++ Picture.java
@@ -16,6 +16,8 @@ public class Picture
     private Triangle roof;
     private Circle sun;
     private Circle sun2;
+    private Person Being;
+    //Question 20
     private boolean drawn;
 
     /**
@@ -28,6 +30,7 @@ public class Picture
         roof = new Triangle();  
         sun = new Circle();
         sun2 = new Circle();
+        Being = new Person();
         drawn = false;
     }
 
@@ -41,30 +44,36 @@ public class Picture
             wall.moveVertical(20);
             wall.changeSize(120);
             wall.makeVisible();
-            
+
             window.changeColor("black");
             window.moveHorizontal(-120);
             window.moveVertical(40);
             window.changeSize(40);
             window.makeVisible();
-    
+
             roof.changeSize(60, 180);
             roof.moveHorizontal(20);
             roof.moveVertical(-60);
             roof.makeVisible();
-    
+
             sun.changeColor("yellow");
             sun.moveHorizontal(100);
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
-            
+
             sun2.changeColor("red");
             sun2.moveHorizontal(-100);
             sun2.moveVertical(40);
             sun2.changeSize(80);
-            sun2.makeVisible();
+           
             
+            Being.changeColor("Black");
+            Being.moveHorizontal(150);
+            Being.moveVertical(30);
+            Being.makeVisible();
+            
+
             drawn = true;
         }
     }
@@ -90,4 +99,17 @@ public class Picture
         roof.changeColor("green");
         sun.changeColor("yellow");
     }
+    
+    /**
+ * The sunset/person  - Question 19/20
+ */
+ public void slowMoveVertical()
+{
+ sun.slowMoveVertical(350);
+ Being.slowMoveHorizontal(-220);
+
 }
+}
+
+
+
